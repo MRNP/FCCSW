a44c63028cb6c918a52e2e89a12b65530156608b
Mohammad Reza Noormandipour <mr.reza@ipm.ir>
2018-12-23 14:42:26 +0330

    Update DelphesSaveMet.cpp

--- Sim/SimDelphesInterface/src/DelphesSaveMet.cpp
+++ Sim/SimDelphesInterface/src/DelphesSaveMet.cpp
@@ -62,15 +62,15 @@ StatusCode DelphesSaveMet::saveOutput(Delphes& delphes, const fcc::MCParticleCol
 
     auto met = colMET->create();
 
-    if (abs(candMET->Momentum.Eta()) < 2.4 && (candMET->Momentum.Pt()) >= 140.0) {
+    //if (abs(candMET->Momentum.Eta()) < 2.4 && (candMET->Momentum.Pt()) >= 140.0) {
         //metCount+=1;
         met.magnitude(candMET->Momentum.Pt());
-        }
-     else 
-     {met.magnitude(-1);}     
+     //   }
+     //else 
+     //{met.magnitude(-1);}     
     
     
-    logging << abs(candMET->Momentum.Eta()) << " " << candMET->Momentum.Pt() << std::endl;
+    logging << abs(candMET->Momentum.Eta()) << " " << candMET->Momentum.Pt() << " " << (-(candMET->Momentum)).Phi() << std::endl;
       
     met.phi((-(candMET->Momentum)).Phi());
     if (saveSHT)
